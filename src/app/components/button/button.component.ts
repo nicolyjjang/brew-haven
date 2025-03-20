f5e05635f7b371bccffd726a5ea1d39130a69715
nicolyjjang <nicolyjjang@gmail.com>
2025-03-20 17:50:28 -0300

    :sparkles: feat: Refatorando o código para melhorar a legibilidade

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,35 +1,39 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   imports: [],
   standalone: true,
   templateUrl: './button.component.html',
-  styleUrl: './button.component.scss'
+  styleUrl: './button.component.scss',
 })
 export class ButtonComponent {
-  @Input() text: string = 'Clique aqui.'
-  @Input() pdfPath: string = ''
-  @Input() sectionId: string = ''
+  @Input() text: string = 'Clique aqui.';
+  @Input() pdfPath: string = '';
+  @Input() sectionId: string = '';
+  @Output() onClick = new EventEmitter<void>();
 
-  handleClick(){
-    if(this.pdfPath){
-      this.openPdf()
+  handleClick() {
+    if (this.pdfPath) {
+      this.openPdf();
+    } else if (this.sectionId) {
+      this.scrollToSection();
+    }
+    this.onClick.emit();
+  }
+
+  openPdf() {
+    if (this.pdfPath) {
+      window.open(this.pdfPath, '_blank');
     }
   }
 
-  openPdf(){
-    if(this.pdfPath){
-      window.open(this.pdfPath, '_blank')
-    }
-  }
+  scrollToSection() {
+    if (this.sectionId) {
+      const element = document.getElementById(this.sectionId);
 
-  scrollToSection(){
-    if(this.sectionId){
-      const element = document.getElementById(this.sectionId)
-
-      if(element){
-        element.scrollIntoView({behavior: 'smooth'})
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
       }
     }
   }
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../components/header/header.component";
+import { FooterComponent } from '../../components/footer/footer.component';
+import { ContactUsComponent } from "../../components/contact-us/contact-us.component";
 
 @Component({
   selector: 'app-contact',
-  imports: [],
   standalone: true,
+  imports: [CommonModule, ContactUsComponent, HeaderComponent, FooterComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
-
-}
+export class ContactComponent { }
